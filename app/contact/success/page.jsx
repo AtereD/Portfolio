fbd7130370d64607bbfd32aae15987116960a733
AtereD <ateredamilola4d@gmail.com>
2025-04-17 02:13:07 +0100

    Style Correction

--- app/contact/success/page.jsx
+++ app/contact/success/page.jsx
@@ -10,7 +10,7 @@ const SuccessMessage = () => {
     <div className='container mx-auto'>
         <div className=" flex flex-col justify-center items-center">
             <div className='flex p-4 bg-[#27272c] rounded-xl items-center justify-center border border-accent-DEFAULT w-[60%] flex-col gap-6 max-w-[450px]'>
-                <p className='text-white/80 text-xl'>Message Sent! I will be in touch..</p>
+                <p className='text-white/80 text-xl'>Message Received!</p>
                 <Link href="/" className='text-4xl text-white/50 hover:text-accent-hover'>
                     <FaHome />
                 </Link>
--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello, i'm <br />{" "}
+              Hello, i&#39;m <br />{" "}
               <span className="text-accent-DEFAULT">Atere Damilola</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
